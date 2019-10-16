3a24f39dec45fb06489d16f21e27e9193b404f93
ChenXP <632469262@qq.com>
2019-10-16 17:58:17 +0800

    加入显示天气信息功能

---
+++ app/src/main/java/com/coolweather/android/gson/Suggestion.java
@@ -0,0 +1,27 @@
+package com.coolweather.android.gson;
+
+import android.nfc.cardemulation.CardEmulation;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Suggestion {
+    @SerializedName("comf")
+    public Comfort comfort;
+    public class Comfort{
+        @SerializedName("txt")
+        public String info;
+    }
+
+    @SerializedName("cw")
+    public CarWash carWash;
+    public class CarWash{
+        @SerializedName("txt")
+        public String info;
+    }
+
+    public Sport sport;
+    public class Sport{
+        @SerializedName("txt")
+        public String info;
+    }
+}
